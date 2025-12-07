0dcb896822b757669d0002ecff703671e9b2d3c0
davaismifadil <davaismif@gmail.com>
2025-12-07 11:54:22 +0700

    Update undo_stack.py

--- src/logic/undo_stack.py
+++ src/logic/undo_stack.py
@@ -1,1 +1,15 @@
+class UndoStack:
+    def __init__(self):
+        self.stack = []
 
+    def push(self, action):
+        self.stack.append(action)
+
+    def pop(self):
+        if self.stack:
+            return self.stack.pop()
+        return None
+
+    def is_empty(self):
+        return len(self.stack) == 0
+      
