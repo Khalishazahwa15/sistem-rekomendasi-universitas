8992483c166e91e8aca8c26d37fc6b3221fdbc66
davaismifadil <davaismif@gmail.com>
2025-12-07 11:53:40 +0700

    Update linked_list.py

--- src/logic/linked_list.py
+++ src/logic/linked_list.py
@@ -1,1 +1,22 @@
+class RecommendationNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
+class RecommendationHistoryLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def push(self, data):
+        node = RecommendationNode(data)
+        node.next = self.head
+        self.head = node
+
+    def to_list(self):
+        result = []
+        curr = self.head
+        while curr:
+            result.append(curr.data)
+            curr = curr.next
+        return result
+      
