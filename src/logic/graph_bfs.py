62fc07a719070b7fa12d9392bdde2d6b01a23509
agnespusvitasari18 <ajjaagnessajja@gmail.com>
2025-12-07 21:16:23 +0700

    Update Kodingan bagian graph,bfs.py
    Implement BFS to find nearest universities in a city.

--- src/logic/graph_bfs.py
+++ src/logic/graph_bfs.py
@@ -1,1 +1,21 @@
+from collections import deque
+from src.data.kota_graph import kota_graph
 
+def find_nearest_city_universities(kota_awal, universitas_per_kota):
+  kota_awal = kota_awal.lower()
+  visited = set()
+  q = deque([kota_awal])
+  visited.add(kota_awal)
+
+  while q:
+    kota = q.popleft()
+
+    if kota in universitas_per_kota:
+      return universitas_per_kota[kota]
+
+      for kota in universitas_per_kota:
+        if tetangga not in visited:
+          visited.add(tetangga)
+          q.append(tetangga)
+
+  return ["Tidak ada data universitas terdekat untuk kota"]
