1dbe4c84eeaef248059d4bf9a06f388d7bbe2be5
Khalishazahwa15 <khalishazahwa360@gmail.com>
2025-12-08 23:36:50 +0700

    Update universitas.py

--- src/data/universitas.py
+++ src/data/universitas.py
@@ -1,12 +1,11 @@
-UNIVERSITAS_PER_KOTA = {
-    "jakarta": ["UI", "BINUS", "Trisakti"],
-    "bandung": ["ITB", "Telkom University"],
-    "yogyakarta": ["UGM", "UNY"],
-    "surabaya": ["UNAIR", "ITS"],
-    "semarang": ["UNDIP"],
-    "palembang": ["Unsri"],
-    "medan": ["USU"],
-    "makassar": ["UNHAS"]
-}
-
-
+universitas_per_kota = {
+            "bandar lampung": ["Universitas Lampung (UNILA)", "Institut Teknologi Sumatera (ITERA)"],
+            "jakarta": ["Universitas Indonesia (UI)", "Universitas Negeri Jakarta (UNJ)", "BINUS University"],
+            "bandung": ["Institut Teknologi Bandung (ITB)", "Universitas Padjadjaran (UNPAD)", "Telkom University"],
+            "yogyakarta": ["Universitas Gadjah Mada (UGM)", "UPN Yogyakarta", "Universitas Islam Indonesia (UII)"],
+            "surabaya": ["Universitas Airlangga (UNAIR)", "Institut Teknologi Sepuluh Nopember (ITS)"],
+            "medan": ["Universitas Sumatera Utara (USU)", "UMSU"],
+            "palembang": ["Universitas Sriwijaya (Unsri)"],
+            "makassar": ["Universitas Hasanuddin (UNHAS)"],
+            "semarang": ["Universitas Diponegoro (UNDIP)"]
+        }
