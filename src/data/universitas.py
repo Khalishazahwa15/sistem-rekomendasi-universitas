b28da6caedd351dd3d738dc0f505bd9ab6843fcc
SivaSinaga <sivasinaga1105@gmail.com>
2025-12-08 08:26:15 +0700

    Add UNIVERSITAS_PER_KOTA dictionary
    Added a dictionary mapping cities to universities.

--- src/data/universitas.py
+++ src/data/universitas.py
@@ -1,1 +1,12 @@
+UNIVERSITAS_PER_KOTA = {
+    "jakarta": ["UI", "BINUS", "Trisakti"],
+    "bandung": ["ITB", "Telkom University"],
+    "yogyakarta": ["UGM", "UNY"],
+    "surabaya": ["UNAIR", "ITS"],
+    "semarang": ["UNDIP"],
+    "palembang": ["Unsri"],
+    "medan": ["USU"],
+    "makassar": ["UNHAS"]
+}
+
 
