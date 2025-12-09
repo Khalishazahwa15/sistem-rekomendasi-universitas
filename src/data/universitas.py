8161203b2f5bb1e4811c8154c7c13d587cf92d2e
SivaSinaga <sivasinaga1105@gmail.com>
2025-12-09 12:00:20 +0700

    Update universitas.py

--- src/data/universitas.py
+++ src/data/universitas.py
@@ -1,11 +1,1 @@
-universitas_per_kota = {
-            "bandar lampung": ["Universitas Lampung (UNILA)", "Institut Teknologi Sumatera (ITERA)"],
-            "jakarta": ["Universitas Indonesia (UI)", "Universitas Negeri Jakarta (UNJ)", "BINUS University"],
-            "bandung": ["Institut Teknologi Bandung (ITB)", "Universitas Padjadjaran (UNPAD)", "Telkom University"],
-            "yogyakarta": ["Universitas Gadjah Mada (UGM)", "UPN Yogyakarta", "Universitas Islam Indonesia (UII)"],
-            "surabaya": ["Universitas Airlangga (UNAIR)", "Institut Teknologi Sepuluh Nopember (ITS)"],
-            "medan": ["Universitas Sumatera Utara (USU)", "UMSU"],
-            "palembang": ["Universitas Sriwijaya (Unsri)"],
-            "makassar": ["Universitas Hasanuddin (UNHAS)"],
-            "semarang": ["Universitas Diponegoro (UNDIP)"]
-        }
+
