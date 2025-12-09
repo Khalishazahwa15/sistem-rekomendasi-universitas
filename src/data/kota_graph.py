820c617135890af9dcdaee491490bb9fc00b20b9
SivaSinaga <sivasinaga1105@gmail.com>
2025-12-09 11:59:49 +0700

    Update kota_graph.py

--- src/data/kota_graph.py
+++ src/data/kota_graph.py
@@ -1,1 +1,12 @@
+kota_graph = {
+    "bandar lampung": ["jakarta"],
+    "jakarta": ["bandar lampung", "bandung", "semarang", "palembang"],
+    "bandung": ["jakarta", "yogyakarta"],
+    "yogyakarta": ["bandung", "surabaya"],
+    "surabaya": ["yogyakarta", "makassar"],
+    "medan": ["palembang"],
+    "palembang": ["medan", "jakarta"],
+    "makassar": ["surabaya"],
+    "semarang": ["jakarta"]
+}
 
